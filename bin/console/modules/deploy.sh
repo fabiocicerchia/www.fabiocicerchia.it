877e194066ded2751d82df7ac1d9e12a98648445
Fabio Cicerchia <info@fabiocicerchia.it>
2012-07-28 23:00:49 +0100

    closed issue #33

---
+++ bin/console/modules/deploy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# FABIO CICERCHIA - WEBSITE
+#
+# Copyright 2012 Fabio Cicerchia.
+#
+# Permission is hereby  granted, free of charge, to any  person obtaining a copy
+# of this software and associated  documentation files (the "Software"), to deal
+# in the Software  without restriction, including without  limitation the rights
+# to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+# copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+# IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+# FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+# AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+# LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Bash Shell
+#
+# Category: Code
+# Package:  Console
+# Author:   Fabio Cicerchia <info@fabiocicerchia.it>
+# License:  MIT <http://www.opensource.org/licenses/MIT>
+# Link:     http://www.fabiocicerchia.it
+#
+
+################################################################################
+# DEPLOY ACTIONS
+################################################################################
+
+deploy_setup() {
+    print_subheader "CAPISTRANO - DEPLOY:SETUP"
+    cap deploy:setup || handle_errors $?
+
+    return $?
+}
+
+deploy_run() {
+    print_subheader "CAPISTRANO - DEPLOY"
+    cap deploy || handle_errors $?
+
+    return $?
+}
